b9c1d1e095564d33eeae56cb803ad913ae73d832
NZESupB <szboyxjw@gmail.com>
2021-05-28 09:15:59 +0800

    Update task.py
    233333

--- task.py
+++ task.py
@@ -103,6 +103,7 @@ def generate(group):
 
     # 获取发言用户数
     user_amount = len(r.hkeys("{}_user_message_amount".format(group)))
+    userid_amount = len(r.hkeys("{}_userid_message_amount".format(group)))
     # 获取所有用户发言数字典
     user_message_amount = r.hgetall("{}_user_message_amount".format(group))
     user_message_amount = sorted(user_message_amount.items(), key=lambda kv: (int(kv[1])), reverse=True)
@@ -133,16 +134,17 @@ def generate(group):
             # print(hot_word_string)
             bot.send_message(
                 chat_id=group,
-                text="🎤 今日话题榜 🎤\n"
+                text="🎤 今日品云榜 🎤\n"
                      "📅 {}\n"
                      "⏱ 截至今天{}\n"
-                     "🗣️ 本群{}位朋友共产生{}条发言\n"
+                     "🗣️ 本群{}个花里胡哨的ID以及真实的{}位水逼共产生{}条发言\n"
                      "🤹‍ 大家今天讨论最多的是：\n\n"
                      "{}\n"
-                     "看下有没有你感兴趣的话题? 👏".format(
+                     "看下你有没有兴趣参与品云击剑大会? 👏".format(
                     time.strftime("%Y年%m月%d日", time.localtime()),
                     time.strftime("%H:%M", time.localtime()),
                     user_amount,
+                    userid_amount,
                     total_message_amount,
                     hot_word_string),
                 parse_mode="Markdown"
@@ -164,12 +166,12 @@ def generate(group):
         # print(top_5_user)
         bot.send_message(
             chat_id=group,
-            text="🏵 今日活跃用户排行榜 🏵\n"
+            text="🏵 品云剑客排行榜 🏵\n"
                  "📅 {}\n"
                  "⏱ 截至今天{}\n\n"
                  "{}\n"
-                 "感谢这些朋友今天的分享! 👏 \n"
-                 "遇到问题,向他们请教说不定有惊喜😃".format(
+                 "感谢这些剑客今天的分享! 👏 \n"
+                 "如果急不可耐,可以考虑🤺哦😃".format(
                 time.strftime("%Y年%m月%d日", time.localtime()),
                 time.strftime("%H:%M", time.localtime()),
                 top_5_user),
