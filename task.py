eda13054f9643de5905c616fae5e07cedfa3a0ca
NZESupB <szboyxjw@gmail.com>
2021-06-26 18:15:54 +0800

    Update task.py
    wordsget

--- task.py
+++ task.py
@@ -92,7 +92,8 @@ def generate(group):
     
     word_list = []
     words = pseg.cut(chat_content, use_paddle=True)  # paddle模式
-    print("准备分析{}").format(words)
+    print("准备分析")
+    print(words)
     for word, flag in words:
         # print(word + "\t" + flag)
         if flag in ["n", "nr", "nz", "PER", "f", "ns", "LOC", "s", "nt", "ORG", "nw"]:
